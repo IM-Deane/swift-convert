72511cfdd8dcd2c08065de63a44aa99f47d8e86c
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-04-03 20:20:43 -0600

    Added web service to image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	staticPageGenerationTimeout: 300,
 	images: {
-		domains: ["tailwindui.com", "images.unsplash.com"],
+		domains: [
+			"tailwindui.com",
+			"images.unsplash.com",
+			"heic-converter.onrender.com",
+		],
 		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 	},
 };
