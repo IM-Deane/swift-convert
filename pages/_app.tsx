2cec502daeac36eb2bf1c618b40b931e7f3e0764
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2024-01-09 10:27:23 -0700

    Cleanup project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import "@/styles/globals.css";
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-
-import * as Fathom from "fathom-client";
+import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
 
@@ -18,7 +15,6 @@ export default function App({
 	Component,
 	pageProps: { session, ...pageProps },
 }) {
-	const router = useRouter();
 	const { settings } = useSettingsContext();
 
 	const [uppy] = useState(
@@ -26,26 +22,6 @@ export default function App({
 			maxTotalFileSize: MaxFileSize.free,
 		})
 	);
-
-	useEffect(() => {
-		// Initialize Fathom when the app loads
-		//  - Do not include https://
-		//  - This must be an exact match of your domain.
-		//  - If you're using www. for your domain, make sure you include that here.
-		Fathom.load("ALZOFJAF", {
-			includedDomains: ["swiftconvert.io", "www.swiftconvert.io"],
-		});
-
-		function onRouteChangeComplete() {
-			Fathom.trackPageview();
-		}
-		// Record a pageview when route changes
-		router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-		return () => {
-			router.events.off("routeChangeComplete", onRouteChangeComplete);
-		};
-	}, [router.events]);
 
 	if (!settings) {
 		return <div>Loading...</div>;
