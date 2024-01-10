345da889a4c752fda483a0d23d7814f4128e8671
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2024-01-10 09:20:47 -0700

    Add posthog analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
 
@@ -11,10 +11,25 @@ import {
 import { createUppyWithTusUploader } from "@/components/UppyDashboard";
 import { MaxFileSize } from "../types";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useRouter } from "next/router";
+
+if (typeof window !== "undefined") {
+	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+		// Enable debug mode in development
+		loaded: (posthog) => {
+			if (process.env.NODE_ENV === "development") posthog.debug();
+		},
+	});
+}
+
 export default function App({
 	Component,
 	pageProps: { session, ...pageProps },
 }) {
+	const router = useRouter();
 	const { settings } = useSettingsContext();
 
 	const [uppy] = useState(
@@ -23,18 +38,27 @@ export default function App({
 		})
 	);
 
+	useEffect(() => {
+		const handleRouteChange = () => posthog?.capture("$pageview");
+		router.events.on("routeChangeComplete", handleRouteChange);
+
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	if (!settings) {
 		return <div>Loading...</div>;
 	}
 
 	return (
-		<>
+		<PostHogProvider client={posthog}>
 			<SessionProvider session={session}>
 				<SettingsProvider>
 					<Toaster />
 					<Component {...pageProps} uppy={uppy} />
 				</SettingsProvider>
 			</SessionProvider>
-		</>
+		</PostHogProvider>
 	);
 }
