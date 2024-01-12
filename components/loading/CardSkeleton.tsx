7437315dd5dd81b79db5d5740ce291aeaf41cbe6
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2024-01-12 09:21:58 -0700

    Update image card and loading skeleton

--- components/loading/CardSkeleton.tsx
+++ components/loading/CardSkeleton.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-
 import ProgressBarSmall from "./ProgressBarSmall";
 
-function CardSkeleton({
+export default function CardSkeleton({
 	imageTitle,
 	progress,
 }: {
@@ -12,23 +11,23 @@ function CardSkeleton({
 	return (
 		<div
 			role="status"
-			className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700"
+			className="w-full h-full overflow-hidden rounded-lg bg-gray-100 animate-pulse"
 		>
-			<div className="flex items-center justify-center h-36 mb-4 bg-gray-300 rounded dark:bg-gray-700">
-				<svg
-					className="w-12 h-12 text-gray-200 dark:text-gray-600"
-					xmlns="http://www.w3.org/2000/svg"
-					aria-hidden="true"
-					fill="currentColor"
-					viewBox="0 0 640 512"
-				>
-					<path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
-				</svg>
+			<div className="flex flex-col justify-between h-full p-4">
+				<div className="aspect-w-10 aspect-h-7 flex justify-center items-center flex-1 mb-2 bg-gray-300 rounded dark:bg-gray-700">
+					<svg
+						className="w-12 h-12 text-gray-200 dark:text-gray-600"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+						fill="currentColor"
+						viewBox="0 0 640 512"
+					>
+						<path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
+					</svg>
+				</div>
+				<ProgressBarSmall title={imageTitle} progress={progress} />
 			</div>
-			<ProgressBarSmall title={imageTitle} progress={progress} />
 			<span className="sr-only">Loading...</span>
 		</div>
 	);
 }
-
-export default CardSkeleton;
