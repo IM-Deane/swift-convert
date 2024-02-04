d2046617f497d7105d77ac962625c745b686ecf9
Tristan Deane <tristandeane93@gmail.com>
2024-02-04 07:40:17 -0700

    Remove commented out options

--- components/SavePhotosDropdown.tsx
+++ components/SavePhotosDropdown.tsx
@@ -103,36 +103,6 @@ export default function SavePhotosDropdown({
 								/>
 							}
 						/>
-						{/* <DropdownOption
-							text="Save to Dropbox"
-							onClick={handleDownloadPhotos}
-							icon={
-								<ArchiveBoxIcon
-									className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-									aria-hidden="true"
-								/>
-							}
-						/>
-						<DropdownOption
-							text="Save to Box"
-							onClick={handleDownloadPhotos}
-							icon={
-								<ArchiveBoxIcon
-									className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-									aria-hidden="true"
-								/>
-							}
-						/>
-						<DropdownOption
-							text="Save to Microsoft OneDrive"
-							onClick={handleDownloadPhotos}
-							icon={
-								<ArchiveBoxIcon
-									className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-									aria-hidden="true"
-								/>
-							}
-						/> */}
 					</div>
 				</Menu.Items>
 			</Transition>
